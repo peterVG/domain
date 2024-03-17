5469378aa875c815004423cc19da51a4127dc21f
Peter Van Garderen <petervangarderen@Peters-MacBook-Air.local>
2024-03-17 10:27:14 -0700

    change to Orcfax colour scheme

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ export default defineConfig({
 	integrations: [
 		starlight({
 			title: 'My Docs',
+			customCss: ['./src/styles/custom.css'],
 			social: {
 				github: 'https://github.com/withastro/starlight',
 			},
